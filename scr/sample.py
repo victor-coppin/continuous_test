133f3f35e1ca9447db8a1f6bc74e37e7766d9177
victor-coppin <victor.coppin@edu.dsti.institute>
2025-03-06 09:52:02 +0100

    change

--- scr/sample.py
+++ scr/sample.py
@@ -1,2 +1,8 @@
-def func(x):
-    return x+1
+def get_email_from_input():
+    """ Contains '@' and '.' """
+    email = input("Tell me your email: ")
+
+    if ("@" not in email or "." not in email):
+        print('Email is not valid.')
+    else:
+        return email
