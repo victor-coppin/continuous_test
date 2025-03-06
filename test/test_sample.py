8f3564340ee84c60e96a5e33befb05eb88210953
victor-coppin <victor.coppin@edu.dsti.institute>
2025-03-06 09:04:18 +0100

    change code : test email and the test related to

--- test/test_sample.py
+++ test/test_sample.py
@@ -1,4 +1,16 @@
 from scr import sample as sml
+import pytest
+import io
+from sample import *
 
-def test_answer():
-    assert sml.func(3) == 5
+def test_email_with_user_input_no_at_sign(monkeypatch):
+    monkeypatch.setattr('sys.stdin', io.StringIO('bruce,wayne2wayneenterprises,com'))
+    assert get_email_from_input() is None
+
+def test_email_with_user_input_no_dot(monkeypatch):
+    monkeypatch.setattr('sys.stdin', io.StringIO('brucewayne@wayneenterprises,com'))
+    assert get_email_from_input() is None
+
+def test_email_with_user_input_correct(monkeypatch):
+    monkeypatch.setattr('sys.stdin', io.StringIO('bruce.wayne@wayneenterprisescom'))
+    assert get_email_from_input() == 'bruce.wayne@wayneenterprisescom'
